0a03ab358968e18f5595fe19caeaac2d7868851f
Cecile <cecile.courrat@gmail.com>
2022-09-28 16:02:35 +0200

    React World

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto libero beatae minima, fugiat voluptate debitis commodi quis? Perspiciatis inventore alias sed doloribus excepturi. Eos, facere corporis quasi dicta doloremque, repellendus sint neque qui, expedita velit vel possimus facilis. Delectus explicabo debitis veniam omnis quas suscipit sunt doloribus temporibus dignissimos. Maxime voluptates quaerat amet iste commodi fugit voluptas dolorem ut quod architecto ad et sapiente laboriosam at earum, laborum deleniti saepe beatae molestiae. Aliquid est consequuntur id voluptates inventore doloribus porro voluptatum, velit nam beatae laborum eaque dolores provident. Minima, nesciunt neque quisquam nam rerum eius minus facilis excepturi similique ipsum!</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus hic, pariatur omnis repudiandae aspernatur enim eos repellendus earum sequi explicabo quasi facilis quia! Nesciunt, sapiente. Animi, maxime ad. Corporis molestias laborum dolores odio magnam, nulla veritatis distinctio quas dignissimos maxime optio est impedit cumque voluptatem, dolorem ea voluptates, aut esse numquam officiis beatae ratione sequi. Non dicta ipsum aut quaerat, laudantium at accusantium possimus facere nobis officiis excepturi iusto nulla delectus vero provident fugit velit ad ratione earum praesentium inventore cumque porro odit! Unde velit eveniet error consequatur saepe quas quos delectus, optio accusamus aliquid modi assumenda quibusdam mollitia, ullam voluptatibus dolores pariatur tempora nihil eligendi. Consequatur sapiente enim aliquid architecto necessitatibus ipsam mollitia nisi unde incidunt minima. Quod quae aut facere architecto maiores quasi facilis nostrum neque incidunt. Amet praesentium iusto, accusamus nulla soluta aspernatur. Quisquam, saepe itaque. Placeat maxime deserunt, corporis corrupti nostrum exercitationem eligendi excepturi fugit facilis. Blanditiis asperiores voluptatem pariatur fugit, laudantium repellendus praesentium. Repellendus iusto excepturi iure, corrupti perferendis labore aut qui ea ipsum dolore quae officia nobis dolores eligendi fugit odit magni aliquam ipsa doloribus tempore voluptatibus cum amet. Incidunt corrupti ab cumque iusto, est dolores dicta atque accusantium, excepturi hic sint, placeat nisi?</p>
+        </div>
+    );
+};
+
+export default About;
